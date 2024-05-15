3918aa1d8de0aa2593d9bd7d0259885b970a6c1d
fridtpup <fridtjof.graaff@student.graafschapcollege.nl>
2024-05-15 11:28:34 +0200

    first commit

---
+++ JS/MenuDrawer.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const menuBtn = document.querySelector('#menu');
+    const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+    const listEl = document.querySelector('.mdc-drawer .mdc-list');
+    const mainContentEl = document.querySelector('.main-content');
+    
+
+    menuBtn.addEventListener('click', () => {
+        drawer.open = true;
+    })
+
+    listEl.addEventListener('click', (event) => {
+        drawer.open = false;
+    });
+
+    document.body.addEventListener('MDCDrawer:closed', () => {
+    mainContentEl.querySelector('input, button').focus();
+    });
+}
